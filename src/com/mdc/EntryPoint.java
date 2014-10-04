52730c55c0980e6ba47796748678d7d19c1a1b88
ProgrammerTony <tony.789@live.com>
2014-10-04 16:30:59 -0400

    Update calculator base

---
+++ src/com/mdc/EntryPoint.java
@@ -0,0 +1,13 @@
+package com.mdc;
+
+import com.mdc.calculator.Calculator;
+
+public class EntryPoint
+{
+	
+	public static void main(String[] args)
+	{
+		Calculator chemistryCalculator = new Calculator(args);
+	}
+
+}
---
+++ src/com/mdc/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.mdc.calculator;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Calculator extends Application
+{
+	
+	public Calculator(String[] args)
+	{
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		
+	}
+
+}
